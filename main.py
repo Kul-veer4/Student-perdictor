2bfcd1dac2ef92a4d2ab9fe2122682770cfb1cce
Kulveer Singh <kulveersingh.thrall@gmail.com>
2026-04-27 02:58:39 +0530

    Added dataset and visulization

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv("data.csv")
+# ======================
+# DAY 1: Load Data
+# ======================
+try:
+    data = pd.read_csv("data.csv")
+except FileNotFoundError:
+    print("Error: data.csv file not found!")
+    exit()
 
-print("Original Data:")
+print("===== Original Data =====")
 print(data)
 
-# Check missing values
-print("\nMissing values:")
+# ======================
+# DAY 2: Data Cleaning
+# ======================
+print("\n===== Missing Values =====")
 print(data.isnull().sum())
 
-# Remove missing values (if any)
+# Remove missing values
 data = data.dropna()
 
-print("\nCleaned Data:")
+print("\n===== Cleaned Data =====")
 print(data)
+
+# ======================
+# DAY 3: Visualization
+# ======================
+plt.figure(figsize=(6,4))
+
+plt.scatter(data["study_hours"], data["marks"])
+
+plt.title("Study Hours vs Marks")
+plt.xlabel("Study Hours")
+plt.ylabel("Marks")
+
+plt.grid(True)
+
+plt.show()
