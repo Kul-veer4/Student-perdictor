7b0a9e4cf04b3d9b62fc2d088c96686215ed92f9
Kulveer Singh <kulveersingh.thrall@gmail.com>
2026-04-27 00:15:45 +0530

    Day 2: data cleaning

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import pandas as pd
 
 data = pd.read_csv("data.csv")
 
-print(data.head())
+print("Original Data:")
+print(data)
+
+# Check missing values
+print("\nMissing values:")
+print(data.isnull().sum())
+
+# Remove missing values (if any)
+data = data.dropna()
+
+print("\nCleaned Data:")
+print(data)
